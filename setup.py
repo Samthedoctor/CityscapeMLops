b5dc12f2b17e49c870d94069b79968023d982082
Samthedoctor <prince.saumya05@gmail.com>
2025-02-16 11:59:40 +0530

    Setup.py completed

--- setup.py
+++ setup.py
@@ -1,1 +1,21 @@
 from setuptools import setup, find_packages
+
+requirements_list=[]
+def requirements()->list:
+    with open('requirements.txt') as f:
+        for line in f:
+            if line.strip()!="-e .":
+                requirements_list.append(line.strip())
+
+    return requirements_list
+
+setup(
+    name='MLops Cityscape',
+    version='0.1.0',
+    author='Saumya Vaidya',
+    author_email="prince.saumya05@gmail.com",
+    long_description=open('README.md').read(),
+    packages=find_packages(),
+    install_requires=requirements(),
+    python_requires='>=3.8'
+)   
