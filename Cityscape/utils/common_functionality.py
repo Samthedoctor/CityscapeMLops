11a9ad2a0714086546a2c78488648907b265f5aa
Samthedoctor <prince.saumya05@gmail.com>
2025-02-19 18:35:59 +0530

    Common functionalites added

--- Cityscape/utils/common_functionality.py
+++ Cityscape/utils/common_functionality.py
@@ -3,6 +3,7 @@ from Cityscape.logging.logger import DualLogger
 from Cityscape.exception.exception import CityscapeException
 import cv2
 import sys
+import shutil
 
 def png_to_jpeg(input_path, output_path):
     logger = DualLogger().get_logger()
@@ -36,10 +37,35 @@ def png_to_jpeg(input_path, output_path):
         logger.error(f"Error occurred while converting PNG to JPEG: {str(e)}", exc_info=True)
         raise CityscapeException(e, sys)
 
+def annotations_to_Dataset(input_path,output_path):
+    logger = DualLogger().get_logger()
+    try:
+        os.makedirs(output_path, exist_ok=True)
+
+        for city in os.listdir(input_path):
+            city_path = os.path.join(input_path, city)
+            output_city_path = os.path.join(output_path, city)
+            os.makedirs(output_city_path, exist_ok=True)
+
+
+            for file in os.listdir(city_path):
+                if file.endswith("_gtfine_color.png"):
+                    input_file_path = os.path.join(city_path, file)
+                    output_file_path = os.path.join(output_city_path, file)
+
+
+                    shutil.copy(input_file_path, output_file_path)
+                    logger.info(f"Successfully copied {input_file_path} → {output_file_path}")
+    
+    
+    
+    except Exception as e:
+        logger.error(f"Error occurred while transferring data: {str(e)}", exc_info=True)
+        raise CityscapeException(e, sys)          
 
 if __name__ == '__main__':
-    input_path = r"D:/MLops project/MLops-cityscape1/Raw Dataset/images/leftImg8bit/val"
-    output_path = r"D:/MLops project/MLops-cityscape1/Dataset/images/leftImg8bit/val"
-    png_to_jpeg(input_path, output_path)
     logger= DualLogger().get_logger()
-    logger.info("PNG to JPEG conversion completed successfully")
+    input_path=r"D:/MLops project/MLops-cityscape1/Raw Dataset/annotations/gtFine/val"
+    output_path=r"D:/MLops project/MLops-cityscape1/Dataset/annotations/gtFine/val"
+    annotations_to_Dataset(input_path, output_path)
+    logger.info("Data transferred successfully")
